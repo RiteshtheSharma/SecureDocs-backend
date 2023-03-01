7d884fba15106cf2097cd37db047af9f30ec870c
RiteshtheSharma <ritesh_@tutanota.com>
2023-03-01 14:37:38 +0530

    added branches for pr practice

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const connectToMongo = require('./db');
 const express = require('express')
 const app = express()
 const port = 3000
+
 connectToMongo();
+// avaliable routes
+app.use('/api/auth',require('./routes/auth'))
+
+// app.use('/api/notes',require('./routes/notes'))
+
+
+/*
+demonstration of app.get 
+
 app.get('/', (req, res) => {
   res.send('Hello icharlie')
 })
-
+*/
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
