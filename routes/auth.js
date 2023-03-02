375acb78d2d054e30ad9812d0a8a724fbc8f5cd8
RiteshtheSharma <ritesh_@tutanota.com>
2023-03-02 20:38:10 +0530

    login end point created

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const User = require("../models/User");
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 const { body, validationResult } = require("express-validator");
+const { findOne } = require("../models/User");
 
 const router = express.Router();
 
@@ -20,6 +21,7 @@ router.post(
   ],
   async (req, res) => {
     console.log(req.body);
+    // validate email and pwd against constraints (if proper email format is followed and pwd 's length >=5 )
     const errors = validationResult(req);
     // if there is errors in credentials return bad request and the errors
     if (!errors.isEmpty()) {
@@ -35,7 +37,7 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with the same Email exists" });
       }
-      //create a user
+      //create a encryption of pwd+ salt
       const salt = await bcrypt.genSalt(10);
       const HashedPwd =await bcrypt.hash(req.body.password,salt)
       user = await User.create({
@@ -44,9 +46,11 @@ router.post(
         password:HashedPwd,
         
       });
+  // creating js object as payload in jwt 
 const data = {
   user:{id:user.id}
 }
+//genetating awt on the basis of signature and data
 const AUTH_TOKEN = jwt.sign(data, process.env.JWT_SECRET)
        res.json({AUTH_TOKEN});
       
@@ -58,7 +62,52 @@ const AUTH_TOKEN = jwt.sign(data, process.env.JWT_SECRET)
 );
 
 //login endpoint
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail().trim().normalizeEmail(),
+    body(
+      "password",
+      "Enter a valid password(must be atleast 5 characters)"
+    ).isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    // validate email and pwd against constraints (if proper email format is followed and pwd 's length >=5 )
+    const errors = validationResult(req);
+    // if there is errors in credentials return bad request and the errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
+     //destructuring sent json in email and password
+      const {email,password} = req.body;
+      try {
+      //finding any user in database  with an email id entered in login details
+      
+      let user = await User.findOne({email:email});
+      //send error response if no user with given email is not found 
+      if(!user){
+        return res
+          .status(400)
+          .json({ error: "Any user with given email Id doesn 't exists" });
+      }
+      const passwordCompare = await bcrypt.compare(password,user.password);
+      if(!passwordCompare){
+        return res.status(400).json({error:'Please provide a valid password for corresponding email id'})
+      }
+      
+const data = {
+  user:{id:user.id}
+}
+const AUTH_TOKEN = jwt.sign(data, process.env.JWT_SECRET)
+       res.json({AUTH_TOKEN});
+      
+    } catch (err) {
+      console.log("error :( ->\n", err.message);
+      res.status(500).json({ errors: "Internal server error" });
+    }
+  }
+);
 
 
 module.exports = router;
