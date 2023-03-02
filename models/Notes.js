f1dc319c6f7d62ad5cc9b007a714dbc71d68066a
RiteshtheSharma <ritesh_@tutanota.com>
2023-03-02 21:49:23 +0530

    Added foreign key to notes collection

--- models/Notes.js
+++ models/Notes.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const NotesSchema = new Schema({
+    // foreign Key which linkes to user collection using its id
+    user : {
+        type: mongoose.Schema.Types.ObjectId ,
+        ref:'user'
+    },
     title: {type:String,required:true},
    
     description:{
--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const Notes = require('../models/Notes');
+const fetchuser = require('../middleware/fetchuser')
 const router = express.Router() 
+//Router 1 : GEt all notes using : GET "api/auth/getuser" . LOgin required
+router.get('/fetchallnotes',fetchuser,async (req,res)=>{
+    try {
 
-router.get('/',(req,res)=>{
-    console.log('\n\n',req.body);
-    const note = Notes(req.body);
-    note.save();
-    res.send(req.body);
+        
+    } catch (error) {
+        console.log("error :( ->\n", err.message);
+      res.status(500).send({ errors: "Internal server error" });
+    }
     })
     module.exports = router
